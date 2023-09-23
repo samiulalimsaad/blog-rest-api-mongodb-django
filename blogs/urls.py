94244d44929c317ae92de69105c65789d0b71d32
samiulalimsaad <samiulalimsaad@gmail.com>
2023-09-23 19:57:39 +0600

    updated

--- blogs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path("index/", views.index),
-    path("index/<str:id>/", views.single),
-]
